b488a7edb19db5394b6f451fee385f134cc410aa
rpuri19 <ruchipuri19@gmail.com>
2025-02-23 02:48:20 +0100

    added masterblog.json

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,8 +1,27 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
+API_URL="/static/masterblog.json" # (2) ensure you create this dir and file
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Masterblog API' # (3) You can change this if you like
+    }
+)
 
 app = Flask(__name__)
+
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
 CORS(app)  # This will enable CORS for all routes
+
+limiter = Limiter(app)
 
 POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
@@ -11,6 +30,7 @@ POSTS = [
 
 
 @app.route('/api/posts', methods=['GET'])
+@limiter.limit("5/minute")
 def get_posts():
     """Fetches all the posts (sorted or original)."""
     # Get the sort and direction query parameters
